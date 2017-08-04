308433853047072d9b2ecc095064a7a547426077
Michelle Poterek <poterekm@gmail.com>
2017-08-04 16:41:46 -0700

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,8 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
-// var sass = require('gulp-sass');
-// var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -88,7 +88,7 @@ gulp.task('serve', ['build'], function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
-  // gulp.watch(["scss/*.scss"], ['cssBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -105,9 +105,9 @@ gulp.task('bowerBuild', ['bower'], function(){
 
 gulp.task('cssBuild', function() {
   return gulp.src(['scss/*.scss'])
-    // .pipe(sourcemaps.init())
-    // .pipe(sass())
-    // .pipe(sourcemaps.write())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());
 });
