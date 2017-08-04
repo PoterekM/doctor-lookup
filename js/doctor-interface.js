63a64a90473659ccbc42095fb723934c071757e1
Michelle Poterek <poterekm@gmail.com>
2017-08-04 14:58:51 -0700

    change for each loop and now recieving console log for result that is not an empty array

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,9 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctor = function(medicalIssue, doctorData) {
-  doctorData.forEach(function(doctor) {
-    $('#results').text(medicalIssue + " " + result.data.profile.last_name);
-    console.log(doctor);
-    console.log(doctorData);
-
+var displayDoctor = function(results, medicalIssue) {
+  results.forEach(function(result) {
+    $('#results').text(medicalIssue + " " + result.data);
+    console.log(result);
   });
   // $('#results').text("the illness you have typed is" + medicalIssue + ". Here are some doctors" + doctorData);
 };
--- js/doctor.js
+++ js/doctor.js
@@ -8,11 +8,7 @@ function Doctor() {
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     var lastName = result.data;
-     displayDoctor(medicalIssue, result.data);
-      console.log(medicalIssue);
-      console.log(lastName);
-      console.log(displayDoctor);
+     displayDoctor(result.data)
 
     })
    .fail(function(error){
