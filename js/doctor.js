1ccdb1675b0cbfe63f23d624d8d7ac342a44e24d
Michelle Poterek <poterekm@gmail.com>
2017-08-04 12:18:47 -0700

    changing doctor.js require to end with .apiKey removes error message of 401 unauthorized

--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,11 @@
-var apiKey = require('./../.env').exports;
+var apiKey = require('./../.env').apiKey;
+///have been changing this from .apiKey to .exports
 
 function Doctor() {
 
 }
 
-Doctor.prototype.getDoctor = function(medicalIssue) { $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctor = function(medicalIssue) { $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
     })
