27f1bda3e75acf11f3c33e01f6b55e1ac8bae078
Michelle Poterek <poterekm@gmail.com>
2017-08-04 15:50:49 -0700

    add empty to clear past results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,8 +2,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(doctors, medicalIssue) {
   doctors.forEach(function(doctor) {
-    $('#results').append(medicalIssue + "<li>" + doctor.profile.last_name + ",   " + doctor.profile.first_name + "</li>");
-    console.log(doctor.profile);
+    $('#results').append("<li>" + doctor.profile.last_name + ",   " + doctor.profile.first_name + "</li>");
   });
   // $('#results').text("the illness you have typed is" + medicalIssue + ". Here are some doctors" + doctorData);
 };
@@ -14,6 +13,7 @@ $(document).ready(function() {
     event.preventDefault();
     var medicalIssue = $('#illness').val();
     $('#illness').val("");
+    $('#results').empty();
     doctorObject.getDoctor(medicalIssue, displayDoctor);
 
   });
--- js/doctor.js
+++ js/doctor.js
@@ -7,8 +7,8 @@ function Doctor() {
 
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result, medicalIssue) {
-     displayDoctor(result.data, medicalIssue);
+   .then(function(result) {
+     displayDoctor(result.data);
 
     })
    .fail(function(error){
