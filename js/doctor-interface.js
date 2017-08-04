d03a595dcf440b0327aa6732a9bef61c74f70445
Michelle Poterek <poterekm@gmail.com>
2017-08-04 15:06:26 -0700

    error status of 429 and too many requests

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,8 +1,9 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(results, medicalIssue) {
-  results.forEach(function(result) {
-    $('#results').text(medicalIssue + " " + result.data);
+  results.forEach(function(doctor) {
+    $('#results').text(medicalIssue + " " + doctor.profile.last_name);
+    console.log(results);
     console.log(result);
   });
   // $('#results').text("the illness you have typed is" + medicalIssue + ". Here are some doctors" + doctorData);
