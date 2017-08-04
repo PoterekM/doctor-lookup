2e7309d06821e5fe7a73694c872d8014cd3662d9
Michelle Poterek <poterekm@gmail.com>
2017-08-04 10:28:42 -0700

    change variable from illness to medical issue to match instructions

--- js/doctor.js
+++ js/doctor.js
@@ -1,9 +1,9 @@
-function Doctor() {
-
-}
-
-Doctor.prototype.getDoctor = function() {
-  console.log("Yes, we are working, captin Sparkle Farts!");
-};
-
-exports.doctorModule = Doctor;
+// function Doctor() {
+//
+// }
+//
+// Doctor.prototype.getDoctor = function() {
+//   console.log("Yes, we are working, captin Sparkle Farts!");
+// };
+//
+// exports.doctorModule = Doctor;
